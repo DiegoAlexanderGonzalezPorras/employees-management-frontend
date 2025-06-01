3079ced155ad303f45fa30f3014f94228187cf04
dgonz66 <dgonz66@bancodebogota.com.co>
2025-06-01 10:39:54 -0500

    SignIn component

--- src/components/signIn/SignInComponent.tsx
+++ src/components/signIn/SignInComponent.tsx
@@ -1,11 +1,45 @@
-import type { ReactElement } from 'react';
-import './SignInComponent.scss'
+import type { ReactElement } from "react";
+import signInImage from "../../assets/signInImage.png";
+import "./SignInComponent.scss";
+import { useNavigate } from "react-router-dom";
 
-const SignInComponent = ():ReactElement => (
-    <>
-        <p>Sign In</p>
-        <button type="button" className="btn btn-primary">Primary</button>
-    </>
-)
+const SignInComponent = (): ReactElement => {
+  const navigate = useNavigate();
+
+  const onSignInButton = () => {
+    navigate("/home");
+  }  
+
+  return (
+    <div className="signIn">
+      <img className="signInImg" src={signInImage} />
+
+      <div className="signInInfo">
+        <p className="h1 signInInfo__title">GESTOR DE EQUIPOS</p>
+        <p className="h4 signInInfo__subtitle">
+          Por favor ingresa a tu cuenta para continuar.
+        </p>
+
+        <div>
+          <p className="h6">Usuario</p>
+          <input className="form-control form-control-lg" type="text" />
+        </div>
+
+        <div>
+          <p className="h6">Contraseña</p>
+          <input className="form-control" type="password" />
+        </div>
+
+        <button
+          type="button"
+          className="btn btn-primary signInInfo__button"
+          onClick={onSignInButton}
+        >
+          Ingresar
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SignInComponent;
