a88649033940e06b575529831bd3a556d45651c5
dgonz66 <dgonz66@bancodebogota.com.co>
2025-06-01 09:32:32 -0500

    Add bootstrap

---
+++ src/components/signIn/SignInComponent.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react';
+import './SignInComponent.scss'
+
+const SignInComponent = ():ReactElement => (
+    <>
+        <p>Sign In</p>
+        <button type="button" className="btn btn-primary">Primary</button>
+    </>
+)
+
+export default SignInComponent;
